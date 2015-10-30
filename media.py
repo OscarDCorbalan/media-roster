afb0d4b7b71d2d83f0803ca4f6f8a3eb547fcfb4
Oscar Dominguez <oscardc@gmx.com>
2015-10-30 17:18:46 +0100

    Added inheritance and preview image fix

--- media.py
+++ media.py
@@ -1,30 +1,27 @@
 import webbrowser
 
 class Video():
-    """This class stores information related to video media"""
+    """This class stores information related to audiovisual media (movies, tv shows...)"""
+
     _VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init(self, video_title, video_duration):
+    def __init__(self, video_title, video_preview, video_duration=None):
         self.title = video_title
         self.self_duration = video_duration
+        self.preview = video_preview
 
-class Movie():
+class Movie(Video):
     """This class stores information related to movies"""
 
-    #def __init__(self, movie_title, movie_duration, movie_trailer_youtube_url, movie_storyline, movie_poster_image_url):
-    def __init__(self, movie_title, movie_trailer_youtube_url, movie_storyline, movie_poster_image_url):
-        #Parent.__init(movie_title, movie_duration, movie_trailer_youtube_url)
-
-        self.title = movie_title
-
+    def __init__(self, movie_title, movie_storyline, movie_poster_image_url, movie_trailer_youtube_url, movie_duration=None):
+        Video.__init__(self, movie_title, movie_trailer_youtube_url, movie_duration)
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_image_url
-        self.trailer_youtube_url = movie_trailer_youtube_url
 
 class TvShow(Video):
     """This class stores information related to TV shows"""
-    def __init(self, show_title, _duration, tvShow_season, tvShow_season_episode, show_tv_station):
-        Parent.__init(show_title, show_duration)
+    def __init__(self, show_title, _duration, tvShow_season, tvShow_season_episode, show_tv_station):
+        Video.__init(show_title, show_duration)
         self.season = show_season
         self.episode = show.episode
         self.tv_station = show_tv_station

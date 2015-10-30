ac1d2e609f1f9ad8981071cc21c45138b946d107
Oscar Dominguez <oscardc@gmx.com>
2015-10-30 16:58:12 +0100

    Initial commit
    Fix data.py return

---
+++ data.py
@@ -0,0 +1,37 @@
+import media
+
+def getMovies():
+    # Create some movies
+    toy_story = media.Movie(
+        "Toy Martian",
+        "During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet.",
+        "http://pics.filmaffinity.com/The_Martian-972637166-large.jpg",
+        "https://www.youtube.com/watch?v=ej3ioOneTy8")
+    avatar = media.Movie(
+        "Battlestar Galactica",
+        "In a distant galaxy lie The Twelve Colonies of Man, a civilization that has been at peace with an empire of machines, the Cylons, who were created as worker drones for mankind, rose in rebellion, and launched war on their masters.",
+        "http://pics.filmaffinity.com/Battlestar_Galactica_TV-925903043-large.jpg",
+        "https://www.youtube.com/watch?v=TnYsf2Yv8i8")
+    torrente = media.Movie(
+        "Torrente",
+        "A corrupt wanna-be cop becomes rich.",
+        "http://i.jeded.com/i/torrente-the-stupid-arm-of-the-law-torrente-el-brazo-tonto-de-la-ley.15154.jpg",
+        "https://www.youtube.com/watch?v=ZC9XPvePksc")
+    interstellar = media.Movie(
+        "Interstellar 2014",
+        "A group of explorers must travel beyond our solar system in search of a planet that can sustain life.",
+        "http://pics.filmaffinity.com/Interstellar-366875261-large.jpg",
+        "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+    star_trek = media.Movie(
+        "Star Trek XI",
+        "The young crew onboard for the maiden voyage of the most advanced starship ever created, the U.S.S. Enterprise, must find a way to stop the evil Nero, whose mission of vengeance threatens all of mankind.",
+        "http://pics.filmaffinity.com/Star_Trek-815589716-large.jpg",
+        "https://www.youtube.com/watch?v=pKFUZ10Wmbw")
+
+    breaveheart = media.Movie(
+        "Braveheart",
+        "Edward the Longshanks, King of England (Patrick McGoohan), has captured Scotlands throne and threatens the freedom of all Scottish people, as tyrannical policies instituted by the English plague the Scots.",
+        "http://pics.filmaffinity.com/Braveheart-898928745-large.jpg",
+        "https://www.youtube.com/watch?v=vBXBtORI7pE")
+
+    return [toy_story, avatar, torrente, interstellar, star_trek, breaveheart]
---
+++ media.py
@@ -0,0 +1,30 @@
+import webbrowser
+
+class Video():
+    """This class stores information related to video media"""
+    _VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init(self, video_title, video_duration):
+        self.title = video_title
+        self.self_duration = video_duration
+
+class Movie():
+    """This class stores information related to movies"""
+
+    def __init__(self, movie_title, movie_duration, movie_trailer_youtube_url, movie_storyline, movie_poster_image_url):
+        #Parent.__init(movie_title, movie_duration, movie_trailer_youtube_url)
+
+        self.title = movie_title
+        self.self_duration = movie_duration
+
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_youtube_url
+
+class TvShow(Video):
+    """This class stores information related to TV shows"""
+    def __init(self, show_title, _duration, tvShow_season, tvShow_season_episode, show_tv_station):
+        Parent.__init(show_title, show_duration)
+        self.season = show_season
+        self.episode = show.episode
+        self.tv_station = show_tv_station

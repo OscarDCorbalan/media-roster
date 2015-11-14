734cda7adfedd2386667ef3847a5b43177ddf5c0
Oscar Dominguez <oscardc@gmx.com>
2015-11-14 20:42:12 +0100

    Add comments

--- js/interactions.js
+++ js/interactions.js
@@ -5,45 +5,46 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-// Make the image big on modal when clicked
+// When clicking an image preview, open it full-screen on a modal
 $(document).on('click', '.media-image', function(event){
-    // Create a new image with the same src
+    // Create a new image with the same src of the clicked one (that is, reference the same image)
     var img = $('<img/>', {
         class: 'width-100',
         src: event.target.src
     });
 
+    // And append it to the modal
     const container = $('#media-extended-container');
     container.empty().append(img);
     container.removeClass('display-none');
 });
 
-// Show detailed media info when the '+' button is clicked
+// When clicking the 'b' glyph/icon, show detailed media info on a modal
 $(document).on('click', '.glyphicon-plus-sign', function(event){
-    // Find the <article> container
+    // Find the <article> container of the icon clicked
     var article = event.target;
     while(article.nodeName.toUpperCase() != 'ARTICLE'){
         article = article.parentNode;
     }
 
-    // Get its media-extension <div>
+    // Get the html in the hidden <div> that contains the detailed info
     const html = $(article).find('.media-info-extended')[0].innerHTML;
 
-    // Append it to the modal
+    // And append its content to the modal
     const container = $('#media-extended-container')
     container.empty().append(html)
     container.removeClass('display-none');
 });
 
-// Start playing the video whenever the trailer modal is opened
+// When clicking the film glyph/icon, open the video in a modal and start playing the video.
 $(document).on('click', '.glyphicon-film', function (event) {
-    // Get the youtube video ID
+    // Get the youtube video ID from the data attribute
     const trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
 
-    // Create the src url
+    // Create the src url for the embedded video. Note the autoplay=1.
     const sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
 
-    // Append it to the modal
+    // Append to the modal an iframe with the embedded video url
     const container = $('#trailer-video-container');
     container.empty().append($('<iframe></iframe>', {
         'id': 'trailer-video',
